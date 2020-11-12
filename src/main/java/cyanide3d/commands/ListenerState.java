fe1e373f81c5419f30e8f0e8821f2c08379e450c
Cyanide3D <72260099+Cyanide3D@users.noreply.github.com>
2020-11-12 06:28:57 +0300

    correct

--- src/main/java/cyanide3d/commands/ListenerState.java
+++ src/main/java/cyanide3d/commands/ListenerState.java
@@ -2,19 +2,28 @@ package cyanide3d.commands;
 
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
+import cyanide3d.Localization;
+import cyanide3d.conf.Permission;
 import cyanide3d.service.EnableActionService;
+import cyanide3d.service.PermissionService;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 
 import java.awt.*;
 
 public class ListenerState extends Command {
+    private final Localization localization = Localization.getInstance();
+
     public ListenerState(){
         this.name = "listenerstate";
     }
 
     @Override
     protected void execute(CommandEvent event) {
+        if (!PermissionService.getInstance().checkPermission(event.getMember(), Permission.MODERATOR)) {
+            event.reply(localization.getMessage("accessDenied", name));
+            return;
+        }
         StringBuilder stringBuilder = new StringBuilder();
         EnableActionService.getInstance().list().stream().forEach(action ->
                 stringBuilder
