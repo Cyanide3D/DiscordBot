2d78a34e1453a82c76bb1461ce3003ecaa0430af
Cyanide3D <gamer1997235@yandex.ru>
2021-04-06 12:24:19 +0300

    wip

--- src/main/java/cyanide3d/handlers/listener/receivedmessage/dialog/DefaultInterceptor.java
+++ src/main/java/cyanide3d/handlers/listener/receivedmessage/dialog/DefaultInterceptor.java
@@ -1,16 +1,41 @@
 package cyanide3d.handlers.listener.receivedmessage.dialog;
 
+import cyanide3d.Localization;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
+import java.util.Random;
+
 public class DefaultInterceptor implements MessageInterceptor{
+
+    private final Localization localization;
+
+    public DefaultInterceptor() {
+        localization = Localization.getInstance();
+    }
+
     @Override
     public void execute(GuildMessageReceivedEvent event) {
+        if (event.getAuthor().isBot()) {
+            return;
+        }
+
+        String message = parseMessage(event);
+        if (!message.isEmpty()) {
+            event.getChannel().sendMessage(message).queue();
+        }
+    }
+
+    private String parseMessage(GuildMessageReceivedEvent event) {
         String message = event.getMessage().getContentRaw();
-        if (!event.getAuthor().isBot() && message.contains("бот") && message.contains("лох")) {
-            event.getChannel().sendMessage(".i. Соси жопу мудак.").queue();
+        String parseMessage = "";
+        if (message.equalsIgnoreCase("бот лох")) {
+            parseMessage = ".i. Соси жопу мудак.";
+        } else if (message.contains("кек")) {
+            parseMessage = "Тише будь, ишь расКЕКался тут.";
+        } else if (message.equalsIgnoreCase("кто я?")) {
+            String[] answers = localization.getMessage("dialog.whoisiam").split("\n");
+            parseMessage = answers[new Random().nextInt(answers.length)];
         }
-        if (!event.getAuthor().isBot() && message.contains("кек")) {
-            event.getChannel().sendMessage("Тише будь, ишь расКЕКался тут.").queue();
-        }
+        return parseMessage;
     }
 }
