44352f20d07181a2addb1c81660ced6d7707eb41
Cyanide3D <gamer1997235@yandex.ru>
2021-04-07 15:09:23 +0300

    wip

--- src/main/java/cyanide3d/commands/basic/WfStatCommand.java
+++ src/main/java/cyanide3d/commands/basic/WfStatCommand.java
@@ -9,43 +9,53 @@ import cyanide3d.util.DefaultAlertMessages;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 public class WfStatCommand extends Command {
 
     private final Localization localization = Localization.getInstance();
-    Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public WfStatCommand() {
         this.name = "wfstat";
     }
+
     @Override
     protected void execute(CommandEvent event) {
         if (event.getArgs() == null) {
-            event.reply(localization.getMessage("command.wfstat.noargs"));
+            event.reply(localization.getMessage("command.wfstat.no.args"));
             return;
         }
         try {
-            WarfacePlayerStats playerInfo = new ObjectMapper().readValue(new URL("http://api.warface.ru/user/stat/?name=" + URLEncoder.encode(event.getArgs(), "utf-8") + "&server=3"), WarfacePlayerStats.class);
-            String[] fullStats = playerInfo.getFullResponse().split("\n");
-            MessageEmbed message = new EmbedBuilder()
-                    .setColor(Color.ORANGE)
-                    .setImage(DefaultAlertMessages.getWfStatImage())
-                    .setThumbnail(event.getGuild().getIconUrl())
-                    .addField(localization.getMessage("command.wfstat.field.stats.title", playerInfo.getNickname()), localization.getMessage("command.wfstat.field.stats.text", playerInfo.getRankId(), playerInfo.getPlaytimeH(), playerInfo.getClanName(), StringUtils.substringAfter(fullStats[3], "="), StringUtils.substringAfter(fullStats[1], "="), StringUtils.substringAfter(fullStats[2], "="), StringUtils.substringAfter(fullStats[0], "="), StringUtils.substringAfter(fullStats[46], "="), StringUtils.substringAfter(fullStats[45], "="), StringUtils.substringAfter(fullStats[14], "=")), false)
-                    .addField(localization.getMessage("command.wfstat.field.stats.pve.title"),localization.getMessage("command.wfstat.field.stats.pve.text",playerInfo.getPve(), playerInfo.getPveAll(), playerInfo.getPveWins(), playerInfo.getPveLost(), playerInfo.getPveKill(), playerInfo.getPveDeath(), playerInfo.getFavoritPVE(), playerInfo.getPveFriendlyKills()), true)
-                    .addField(localization.getMessage("command.wfstat.field.stats.pvp.title"), localization.getMessage("command.wfstat.field.stats.pvp.text", playerInfo.getPvp(), playerInfo.getPvpAll(), playerInfo.getPvpWins(), playerInfo.getPvpLost(), playerInfo.getKills(), playerInfo.getDeath(), playerInfo.getFavoritPVP()), true)
-                    .build();
+            MessageEmbed message = getMessageWithStats(event);
             event.reply(message);
         } catch (IOException e) {
-            logger.error("Some bug with warface stats ", e);
-            event.reply(localization.getMessage("command.wfstat.no"));
+            errorHandle(event);
         }
+    }
+
+    @NotNull
+    private MessageEmbed getMessageWithStats(CommandEvent event) throws IOException {
+        String nickname = event.getArgs();
+        WarfacePlayerStats playerInfo = new ObjectMapper().readValue(new URL("http://api.warface.ru/user/stat/?name=" + URLEncoder.encode(nickname, StandardCharsets.UTF_8) + "&server=3"), WarfacePlayerStats.class);
+        String[] fullStats = playerInfo.getFullResponse().split("\n");
+
+        return new EmbedBuilder()
+                .setColor(Color.ORANGE)
+                .setImage(DefaultAlertMessages.getWfStatImage())
+                .setThumbnail(event.getGuild().getIconUrl())
+                .addField(localization.getMessage("command.wfstat.field.stats.title", playerInfo.getNickname()), localization.getMessage("command.wfstat.field.stats.text", playerInfo.getRankId(), playerInfo.getPlaytimeH(), playerInfo.getClanName(), StringUtils.substringAfter(fullStats[3], "="), StringUtils.substringAfter(fullStats[1], "="), StringUtils.substringAfter(fullStats[2], "="), StringUtils.substringAfter(fullStats[0], "="), StringUtils.substringAfter(fullStats[46], "="), StringUtils.substringAfter(fullStats[45], "="), StringUtils.substringAfter(fullStats[14], "=")), false)
+                .addField(localization.getMessage("command.wfstat.field.stats.pve.title"), localization.getMessage("command.wfstat.field.stats.pve.text", playerInfo.getPve(), playerInfo.getPveAll(), playerInfo.getPveWins(), playerInfo.getPveLost(), playerInfo.getPveKill(), playerInfo.getPveDeath(), playerInfo.getFavoritPVE(), playerInfo.getPveFriendlyKills()), true)
+                .addField(localization.getMessage("command.wfstat.field.stats.pvp.title"), localization.getMessage("command.wfstat.field.stats.pvp.text", playerInfo.getPvp(), playerInfo.getPvpAll(), playerInfo.getPvpWins(), playerInfo.getPvpLost(), playerInfo.getKills(), playerInfo.getDeath(), playerInfo.getFavoritPVP()), true)
+                .build();
+    }
+
+    private void errorHandle(CommandEvent event) {
+        event.reply(localization.getMessage("command.wfstat.no"));
     }
 }
--- src/main/java/cyanide3d/commands/moderation/settings/TestCommand.java
+++ src/main/java/cyanide3d/commands/moderation/settings/TestCommand.java
@@ -2,11 +2,6 @@ package cyanide3d.commands.moderation.settings;
 
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
-import cyanide3d.repository.service.PunishmentService;
-import cyanide3d.util.Punishment;
-import net.dv8tion.jda.api.MessageBuilder;
-import net.dv8tion.jda.api.entities.Message;
-import org.apache.maven.shared.utils.logging.MessageUtils;
 
 public class TestCommand extends Command {
 
